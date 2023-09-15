796f4aee65842f7315eada3a5406742701ad6b19
hisaki <alltimelow2011@gmail.com>
2023-09-15 18:54:59 +0900

    setting rspec

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,16 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    config.generators do |g|
+      g.test_framework  :rspec,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        request_specs: false
+      g.factory_bot false
+      g.factory_bot dir: "spec/factories"
+    end
     
     # Configuration for the application, engines, and railties goes here.
     #
