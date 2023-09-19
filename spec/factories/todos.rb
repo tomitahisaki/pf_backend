cebc4e888bd91ad8c3a2703022c4391011233a73
hisaki <alltimelow2011@gmail.com>
2023-09-19 18:01:08 +0900

    add rspec models/todo

--- spec/factories/todos.rb
+++ spec/factories/todos.rb
@@ -1,7 +1,15 @@
 FactoryBot.define do
   factory :todo do
-    title { "MyString" }
-    description { "MyString" }
-    status { "MyString" }
+    title { "勉強中です" }
+    description { "プログラミング学習です" }
+    status { :not_started }
+  end
+
+  trait :done do
+    status { :done }
+  end
+
+  trait :in_progress do
+    status { :in_progress }
   end
 end
